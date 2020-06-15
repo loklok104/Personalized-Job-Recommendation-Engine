bea4871f35e1e4c61c5bf9a140c3d89126c6b066
loklok104 <linglok1230@gmail.com>
2020-06-14 17:30:32 -0700

    second commit

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db;
 
 public class MySQLDBUtil {
-	private static final String INSTANCE = "laiproject-instance-test.cztr8itiovqi.us-east-2.rds.amazonaws.com";
+	private static final String INSTANCE = "INSTANCE_ADDRESS";
 	private static final String PORT_NUM = "3306";
-	public static final String DB_NAME = "laiproject";
+	public static final String DB_NAME = "DB_NAME";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "12345678";
+	private static final String PASSWORD = "PASSWORD";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
